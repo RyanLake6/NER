0d1c4ecdc484485355dec9feae1fd85304ba29d8
joshua2020181 <cheng.jo@northeastern.edu>
2020-10-14 12:56:19 -0400

    Update RTDS.ino

--- ArduinoCode/Driver-IO/RTDS/RTDS/RTDS.ino
+++ ArduinoCode/Driver-IO/RTDS/RTDS/RTDS.ino
@@ -1,68 +1,67 @@
-//By Joshua Cheng
-//Oct 12, 2020
-
-/* detects shutdown voltage, if voltage is OK (12v scaled down) allow button (turn on button LED) 
- *  to turn speaker on for three seconds 
- */
+// Author: Joshua Cheng
+// Date: October 12, 2020
 
 bool shutdownVoltage = false;
-const int SHUTDOWN_DETECTION_PIN = 3; //pin for detecting shutdown system voltage
-
-const int BUTTON = 2; 
-const int BUTTON_LED = 11;
-const int SPEAKER = 10; //must be pwm pin
+const int SHUTDOWN_DETECTION_PIN = 3; // Pin for detecting shutdown system voltage.
+const int BUTTON = 2; // Pin for on/off button.
+const int BUTTON_LED = 11; // Pin for integrated LED on the button.
+const int SPEAKER = 10; // Pin for speaker or relay for speaker (must be pwm pin).
 long speakerCooldown = 0;
 long buttonCooldown = 0;
 bool state = false;
 
-
-
+/// Sets relavent pins to output or input, initializes serial for debugging.
 void setup()
 {
   Serial.begin(115200);
 
   pinMode(SHUTDOWN_DETECTION_PIN, INPUT);
-  pinMode(BUTTON, INPUT_PULLUP); //built in pullup resistor
+  pinMode(BUTTON, INPUT_PULLUP); // Uses built-in pullup resistor.
 
   pinMode(SPEAKER, OUTPUT);
   pinMode(BUTTON_LED, OUTPUT);
-  
 }
 
-
+/** 
+ *  On/off button with RTDS.
+ *  
+ *  Will only set vehicle to "ready to drive" if shutdown circuit is enabled and button is pressed.
+ *  Button LED is activated when shutdown circuit is detected. Speaker will activate for 3 seconds when
+ *  the vehicle becomes "ready to drive".
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
-  shutdownVoltage = digitalRead(SHUTDOWN_DETECTION_PIN); //detects if pin has 5v (divided from 12) 
+  shutdownVoltage = digitalRead(SHUTDOWN_DETECTION_PIN); // Detects if shutdown circuit is active (5v divided from 12v). 
 
-  if(shutdownVoltage){
+  // If shutdown circuit is active, turn on the LED. Otherwise, turn LED and speaker off, and set state to off
+  if (shutdownVoltage) {
     digitalWrite(BUTTON_LED, HIGH);
-  }
-  else{
+  } else {
     digitalWrite(BUTTON_LED, LOW);
     state = 0;
     speakerCooldown = millis();
   }
   
-
-  if(speakerCooldown < millis() || !state){
-    //Serial.println("Speaker Off");
+  if (speakerCooldown < millis() || !state) {
+    // Serial.println("Speaker Off");
     analogWrite(SPEAKER, 0);
   }
 
-  if(shutdownVoltage && buttonCooldown < millis() && digitalRead(BUTTON) == LOW){ //button pressed
+  /* If the shutdown circuit is active, button cooldown has passed, and button is pressed,
+   * toggle state, reset button cooldown. If state is true, turn on speaker.
+   */
+  if (shutdownVoltage && buttonCooldown < millis() && digitalRead(BUTTON) == LOW) {
     state = !state;
-    buttonCooldown = millis() + 500;
-    if(state){
+    buttonCooldown = millis() + 500; // Set cooldown to 500ms (prevents triggering button multiple times with one press).
+    if (state) {
       analogWrite(SPEAKER, 128);
-      //Serial.println("Speaker On");
+      // Serial.println("Speaker On");
       speakerCooldown = millis() + 3000;
     }
   }
   
   String s = "off";
-  if(state){
+  if (state) {
     s = "on";
   }
   Serial.println("STATE: " + s + "  |  Shutdown Circuit Detection: " + String(shutdownVoltage));
-
 }
