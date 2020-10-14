67d8ee723c4b2a08070d053b2fa369227c390e83
rydzynskip <rydzynski.p@gmail.com>
2020-10-13 21:04:33 -0400

    Updated Pedal Arduino Code
    Updated code for only two potentiometers and a brake switch.

--- ArduinoCode/Pedal Potentiometers/Pedal_Potentiometers/Pedal_Potentiometers.ino
+++ ArduinoCode/Pedal Potentiometers/Pedal_Potentiometers/Pedal_Potentiometers.ino
@@ -2,36 +2,35 @@
 #include <mcp_can_dfs.h>
 #include <SPI.h>
 
-//Being adapted by Michael Zappa for pedal potentiometers
-int brakePin1 = A0; 
-int brakePin2 = A1; 
-int gasPin1 = A2; 
-int gasPin2 = A3; 
+/**TODO:
+     * Update message formats for what is expected in motor controller
+     * Change CAN Ids to expected values
+*/
 
-int brakePin1Val = 0; 
-int brakePin2Val = 0; 
+//potentiometer analog pins
+int gasPin1 = A0; 
+int gasPin2 = A1; 
+
+//brake light switch digital pins
+int brakePin1 = 2;
+int brakePin2 = 3;
+
+//brake switch and potentiometer values
 int gasPin1Val = 0; 
 int gasPin2Val = 0;
-
-String brakePin1ValString = ""; 
-String brakePin2ValString = ""; 
-String gasPin1ValString = ""; 
-String gasPin2ValString = "";
+int brakePin1Val;
+int brakePin2Val;
  
 // CAN stuff
 
-const int myCAN_brake = 0x01; //device's CAN id
-const int myCAN_brake_broke = 0x02; // device error ID
-const int myCAN_gas = 0x03; //device's CAN id
-const int myCAN_gas_broke = 0x04; // device error ID
+const int myCAN_gas = 0x01; //device's CAN id
+const int myCAN_gas_broke = 0x02; // device error ID
+const int myCAN_brake = 0x03; //device's CAN id
+const int myCAN_brake_broke = 0x04; // device error ID
 
 const int spiCSPin = 10; //base CAN pin
 unsigned char errorMessage[8] = {0, 255, 0, 0, 0, 0, 0, 0}; //output message for when the potentiometers are too far apart
 // this message can be changed, but for now has value in second index as no "good" message will have a value there
-
-unsigned char len = 0; //length of incoming message
-unsigned char buf[8]; //memory for incoming message
-int canId;
 
 MCP_CAN CAN(spiCSPin); //setup can device
 void setup()
@@ -44,26 +43,16 @@ void setup()
     delay(250); //delay, retry
     }
   Serial.println("CAN BUS Shield Init OK!");
+
+  pinMode(brakePin1, INPUT);
+  pinMode(brakePin2, INPUT);
 }
 
 void loop() {
-  // reading potentiometer values for each pedal, comparing
+  // reading potentiometer values for gas pedal, comparing
   // them and then sending a message to the CAN
-  brakePin1Val = analogRead(brakePin1); 
-  brakePin2Val = analogRead(brakePin2); 
   gasPin1Val = analogRead(gasPin1); 
   gasPin2Val = analogRead(gasPin2);
-
-  if(abs(brakePin1Val-brakePin2Val) <= 1024*0.1){
-    int averageReading = (brakePin1Val + brakePin2Val)/2;
-    unsigned char byteReading = averageReading/4;
-    unsigned char valueMessage[8] = {byteReading, 0, 0, 0, 0, 0, 0, 0};
-    
-    CAN.sendMsgBuf(myCAN_brake, 0, 8, valueMessage); //send message of the average value of the two brake potentiometers
-  }
-  else{
-    CAN.sendMsgBuf(myCAN_brake_broke, 0, 8, errorMessage); //send message that the two potentiometer values are too far apart
-  }
 
   if(abs(gasPin1Val-gasPin2Val) <= 1024*0.1){
     int averageReading = (gasPin1Val + gasPin2Val)/2;
@@ -75,7 +64,20 @@ void loop() {
   else{
     CAN.sendMsgBuf(myCAN_gas_broke, 0, 8, errorMessage); //send message that the two potentiometer values are too far apart
   }
-  
+
+  // reading switch values for brake pedal, comparing
+  // them and then sending a message to the CAN
+  brakePin1Val = digitalRead(brakePin1);
+  brakePin2Val = digitalRead(brakePin2);
+
+  if(brakePin1Val == LOW && brakePin2Val == LOW){
+    unsigned char valueMessage[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+    
+    CAN.sendMsgBuf(myCAN_brake, 0, 8, valueMessage); //send message that the switches are turned on
+  }
+  else if(brakePin1Val != brakePin2Val){
+    CAN.sendMsgBuf(myCAN_brake_broke, 0, 8, errorMessage); //send message that the two switch values are not equal
+  }
 
   
 }
