6ca35f3e08660b9360a7512a49a62a7967fef668
RyanLake6 <73082037+RyanLake6@users.noreply.github.com>
2020-11-03 20:02:43 -0500

    Update pedalbox CAN

--- ArduinoCode/Pedal_Potentiometers/Pedal_Potentiometers.ino
+++ ArduinoCode/Pedal_Potentiometers/Pedal_Potentiometers.ino
@@ -1,14 +1,5 @@
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
-
-/**
- * TODO:
- *  Update message formats for what is expected in motor controller 
- *  Change CAN Ids to expected values  
-*/
-
-#include <mcp_can.h>
-#include <mcp_can_dfs.h>
 #include <SPI.h>
 
 // potentiometer analog pins
@@ -30,6 +21,10 @@ const int CAN_ACCEL = 0x01;       // device's CAN id
 const int CAN_ACCEL_BROKE = 0x02; // device error ID
 const int CAN_BRAKE = 0x03;       // device's CAN id
 const int CAN_BRAKE_BROKE = 0x04; // device error ID
+
+//Dashboard variables
+unsigned char len = 0;  //length of the data in the buffer
+unsigned char buf[1];  //received data (only 1 byte)
 
 const int SPI_CS_PIN = 10; // base CAN pin
 unsigned char errorMessage[8] = {0, 255, 0, 0, 0, 0, 0, 0}; // output message for when the potentiometers are too far apart
@@ -95,3 +90,34 @@ void readSwitches() {
     CAN.sendMsgBuf(CAN_BRAKE_BROKE, 0, 8, errorMessage); // send message that the two switch values are not equal
   }
 }
+
+void readDashboard() {
+    if(CAN_MSGAVAIL == CAN.checkReceive()) {   //if a new message has been recieved. 
+      CAN.readMsgBuf(&len, buf); //enters message into program
+      canId = CAN.getCanId(); //gets sender ID
+     if (canID == 0x01) {  //determine whether Dashboard is on or off
+      if (buf[0] == 0) {
+       // code for when dashboad is off
+      }
+      else if (buf[0] == 1) {
+       // code for when dashboard is on
+      }
+     }
+     
+     else if (canID == 0x02) {  //determine whether forward or revers
+      if (buf[0] == 0) {
+       // code for when in reverse
+      }
+      else if (buf[0] == 1) {
+       // code for when forward
+      }
+     }
+    }
+}
+
+void MsgMotorController(int torque, int direction, int inverter) {  //torque must be 0-255, direction 0 is reverse, 1 is forward, inverter 0 is off, 1 is on
+ unsigned char valueMessage[8] = {torque, 0, 0, direction, inverter, 0, 0, 0};
+ CAN.sendMsgBuf(CAN_MOTOR, 0, 8, valueMessage);
+}
+
+ 
